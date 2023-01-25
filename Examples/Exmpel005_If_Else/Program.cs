8cc89ae91053534a11192801c86bd072803ae846
Ignat lazinkov <lazinkovignat@gmail.com>
2023-01-25 21:16:36 +0300

    ifElse_Приветствие

---
+++ Examples/Exmpel005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввидите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "игнат")
+{
+   Console.WriteLine("Где ты был? Мы тебя заждались!");
+}
+else
+{
+   Console.Write("А ты кто?");
+   Console.WriteLine(username);
+}
